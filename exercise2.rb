98657dc8f688a48d45698fc50c8d6f486f1f032b
Hira Kazmi <hirarukh@gmail.com>
2016-05-31 14:48:50 -0400

    finished exercise2

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+# 1. How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+puts 55 * 0.13
+
+puts "I am #{5} years old"
+
+puts "Lies, I am actually #{45628 * 7839} years old"
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
